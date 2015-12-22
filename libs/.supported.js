25568688adf32ce2bd67254566fae511ce4de106
budparr <budparr@sonnetmedia.net>
2015-12-21 23:04:43 -0500

    clean up branch
    Signed-off-by: budparr <budparr@sonnetmedia.net>

--- libs/.supported.js
+++
@@ -1,4 +0,0 @@
-supportedJSONPCallback([
-  'scaffolding', 'scaffolding_force', 'check_scaffolding', 'reset_files', 'supported_messages',
-  'push', 'build', 'preset', 'layouts', 'preset_localv2', 'generate_slug_v2'
-]);
