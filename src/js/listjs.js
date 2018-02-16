72468a81efb27cbd9232ceff2b1d64366223014c
budparr <budparr@gmail.com>
2018-02-16 13:59:24 -0500

    [wip] Add filter buttons to tools pages

--- src/js/listjs.js
+++ src/js/listjs.js
@@ -1,20 +1,28 @@
-//var listjs = require('list.js/dist/list.min.js');
-var List = require('list.js');
+var List = require("list.js");
+var options = {
+  valueNames: ["js-title", "category", "js-description", "js-main-category"],
+  listClass: "js-filteredlist"
+};
+var tools = new List("filter", options);
+console.log(tools)
+var filterBtns = document.getElementsByClassName("js-filter-buttons");
 
-
-var libraryFilters = {
-  valueNames: ['title', 'isbn', 'author', 'date', 'genre'],
-  listClass: 'filteredlist'
+var filters = Array.prototype.filter.call(filterBtns, function(filterBtn) {
+  var category = filterBtn.id;
+  console.log(filterBtn.id)
+  filterBtn.onclick = function() {
+    tools.filter(function(item) {
+      if (item.values().category == category) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  };
+});
+//and clear the filters
+if (document.getElementById("clear-filters")) {
+    document.getElementById("clear-filters").onclick=function(){
+    tools.filter();
+  };
 }
-
-
-var library = new List('filter', libraryFilters)
-
-
-
-// //and clear the filters
-// if (document.getElementById("filter-none")) {
-//     document.getElementById("filter-none").onclick=function(){
-//     authorList.filter();
-//   };
-// }
--- src/js/main.js
+++ src/js/main.js
@@ -3,5 +3,6 @@ import fonts from './../fonts.css';
 
 
 import './lazysizes.js'
+import './listjs.js';
 import './turbolinks.js';
 import './nojs.js'
