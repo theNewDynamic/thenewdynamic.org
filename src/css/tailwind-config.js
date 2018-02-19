00b0afce4effc4df863582751bfbf3bcff5090f4
budparr <budparr@gmail.com>
2018-02-19 11:48:13 -0500

    Add red to css

--- src/css/tailwind-config.js
+++ src/css/tailwind-config.js
@@ -73,7 +73,7 @@ let colors = {
   // 'red-darkest': '#3b0d0c',
   // 'red-darker': '#621b18',
   // 'red-dark': '#cc1f1a',
-  // 'red': '#e3342f',
+   'red': '#e3342f',
   // 'red-light': '#ef5753',
   // 'red-lighter': '#f9acaa',
   // 'red-lightest': '#fcebea',
