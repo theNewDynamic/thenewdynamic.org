1af4373c8936ac5de1524d0daa82a7e6f1c6af31
budparr <budparr@gmail.com>
2018-02-23 09:25:19 -0500

    Add back some colors

--- src/css/tailwind-config.js
+++ src/css/tailwind-config.js
@@ -26,7 +26,6 @@ View the full documentation at https://tailwindcss.com.
 
 //let defaultConfig = require('tailwindcss/defaultConfig')()
 
-
 /*
 |-------------------------------------------------------------------------------
 | Colors                                    https://tailwindcss.com/docs/colors
@@ -44,36 +43,35 @@ View the full documentation at https://tailwindcss.com.
 */
 
 let colors = {
-  'transparent': 'transparent',
+  transparent: "transparent",
 
-
-  'primary-color':        '#8c1613',
+  "primary-color": "#8c1613",
   //'primary-color-dark':   ,
   //'primary-color-light':  ,
   //'accent-color':         ,
   //'accent-color-light':   ,
   //'accent-color-dark':    ,
-  'text-color-primary':   '#373737',
+  "text-color-primary": "#373737",
   //'text-on-primary-color': ,
   //'text-color-secondary': ,
   //'text-color-disabled':  ,
   //'divider-color':        ,
   //'warn-color': var(--dark-red);
 
-  'black': '#22292f',
-  'grey-darkest': '#3d4852',
-  'grey-darker': '#606f7b',
-  'grey-dark': '#8795a1',
-  'grey': '#b8c2cc',
-  'grey-light': '#dae1e7',
-  'grey-lighter': '#f1f5f8',
-  'grey-lightest': '#f8fafc',
-  'white': '#ffffff',
+  black: "#22292f",
+  "grey-darkest": "#3d4852",
+  "grey-darker": "#606f7b",
+  "grey-dark": "#8795a1",
+  grey: "#b8c2cc",
+  "grey-light": "#dae1e7",
+  "grey-lighter": "#f1f5f8",
+  "grey-lightest": "#f8fafc",
+  white: "#ffffff",
 
   // 'red-darkest': '#3b0d0c',
   // 'red-darker': '#621b18',
   // 'red-dark': '#cc1f1a',
-   'red': '#e3342f',
+  red: "#e3342f",
   // 'red-light': '#ef5753',
   // 'red-lighter': '#f9acaa',
   // 'red-lightest': '#fcebea',
@@ -110,13 +108,13 @@ let colors = {
   // 'teal-lighter': '#a0f0ed',
   // 'teal-lightest': '#e8fffe',
   //
-  // 'blue-darkest': '#12283a',
-  // 'blue-darker': '#1c3d5a',
-  // 'blue-dark': '#2779bd',
-  // 'blue': '#3490dc',
-  // 'blue-light': '#6cb2eb',
-  // 'blue-lighter': '#bcdefa',
-  // 'blue-lightest': '#eff8ff',
+  "blue-darkest": "#12283a",
+  "blue-darker": "#1c3d5a",
+  "blue-dark": "#2779bd",
+  blue: "#3490dc",
+  "blue-light": "#6cb2eb",
+  "blue-lighter": "#bcdefa",
+  "blue-lightest": "#eff8ff"
   //
   // 'indigo-darkest': '#191e38',
   // 'indigo-darker': '#2f365f',
@@ -141,10 +139,9 @@ let colors = {
   // 'pink-light': '#fa7ea8',
   // 'pink-lighter': '#ffbbca',
   // 'pink-lightest': '#ffebef',
-}
+};
 
 module.exports = {
-
   /*
   |-----------------------------------------------------------------------------
   | Colors                                  https://tailwindcss.com/docs/colors
@@ -159,7 +156,6 @@ module.exports = {
   */
 
   colors: colors,
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -181,12 +177,11 @@ module.exports = {
   */
 
   screens: {
-    'sm': '576px',
-    'md': '768px',
-    'lg': '992px',
-    'xl': '1200px',
+    sm: "576px",
+    md: "768px",
+    lg: "992px",
+    xl: "1200px"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -207,22 +202,22 @@ module.exports = {
   */
 
   fonts: {
-    'sans': [
-      'Avenir Next W00',
-      'Avenir Next',
-      'Avenir',
-      'system-ui',
-      'BlinkMacSystemFont',
-      '-apple-system',
-      'Segoe UI',
-      'Roboto',
-      'Oxygen',
-      'Ubuntu',
-      'Cantarell',
-      'Fira Sans',
-      'Droid Sans',
-      'Helvetica Neue',
-      'sans-serif',
+    sans: [
+      "Avenir Next W00",
+      "Avenir Next",
+      "Avenir",
+      "system-ui",
+      "BlinkMacSystemFont",
+      "-apple-system",
+      "Segoe UI",
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+      "sans-serif"
     ],
     // ,
     // 'serif': [
@@ -246,24 +241,23 @@ module.exports = {
     //   'Courier New',
     //   'monospace',
     // ]
-    'brand':[
-      'URW Franklin Gothic W01',
-      'URW Franklin Gothic',
-      'system-ui',
-      'BlinkMacSystemFont',
-      '-apple-system',
-      'Segoe UI',
-      'Roboto',
-      'Oxygen',
-      'Ubuntu',
-      'Cantarell',
-      'Fira Sans',
-      'Droid Sans',
-      'Helvetica Neue',
-      'sans-serif',
+    brand: [
+      "URW Franklin Gothic W01",
+      "URW Franklin Gothic",
+      "system-ui",
+      "BlinkMacSystemFont",
+      "-apple-system",
+      "Segoe UI",
+      "Roboto",
+      "Oxygen",
+      "Ubuntu",
+      "Cantarell",
+      "Fira Sans",
+      "Droid Sans",
+      "Helvetica Neue",
+      "sans-serif"
     ]
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -285,17 +279,16 @@ module.exports = {
   */
 
   textSizes: {
-    'xs': '.75rem',     // 12px
-    'sm': '.875rem',    // 14px
-    'base': '1rem',     // 16px
-    'lg': '1.125rem',   // 18px
-    'xl': '1.25rem',    // 20px
-    '2xl': '1.5rem',    // 24px
-    '3xl': '1.875rem',  // 30px
-    '4xl': '2.25rem',   // 36px
-    '5xl': '3rem',      // 48px
+    xs: ".75rem", // 12px
+    sm: ".875rem", // 14px
+    base: "1rem", // 16px
+    lg: "1.125rem", // 18px
+    xl: "1.25rem", // 20px
+    "2xl": "1.5rem", // 24px
+    "3xl": "1.875rem", // 30px
+    "4xl": "2.25rem", // 36px
+    "5xl": "3rem" // 48px
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -315,14 +308,13 @@ module.exports = {
     // 'hairline': 100,
     // 'thin': 200,
     // 'light': 300,
-    'normal': 400,
-    'medium': 500,
-    'semibold': 600,
-    'bold': 700,
-    'extrabold': 800,
+    normal: 400,
+    medium: 500,
+    semibold: 600,
+    bold: 700,
+    extrabold: 800
     // 'black': 900,
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -337,12 +329,11 @@ module.exports = {
   */
 
   leading: {
-    'none': 1,
-    'tight': 1.25,
-    'normal': 1.5,
+    none: 1,
+    tight: 1.25,
+    normal: 1.5
     // 'loose': 2,
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -357,11 +348,10 @@ module.exports = {
   */
 
   tracking: {
-    'tight': '-0.05em',
-    'normal': '0',
-    'wide': '0.05em',
+    tight: "-0.05em",
+    normal: "0",
+    wide: "0.05em"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -378,7 +368,6 @@ module.exports = {
 
   textColors: colors,
 
-
   /*
   |-----------------------------------------------------------------------------
   | Background colors             https://tailwindcss.com/docs/background-color
@@ -394,7 +383,6 @@ module.exports = {
 
   backgroundColors: colors,
 
-
   /*
   |-----------------------------------------------------------------------------
   | Border widths                     https://tailwindcss.com/docs/border-width
@@ -409,13 +397,12 @@ module.exports = {
   */
 
   borderWidths: {
-    default: '1px',
-    '0': '0',
-    '2': '2px',
-    '4': '4px',
-    '8': '8px',
+    default: "1px",
+    "0": "0",
+    "2": "2px",
+    "4": "4px",
+    "8": "8px"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -434,8 +421,7 @@ module.exports = {
   |
   */
 
-  borderColors: Object.assign({ default: colors['grey-light'] }, colors),
-
+  borderColors: Object.assign({ default: colors["grey-light"] }, colors),
 
   /*
   |-----------------------------------------------------------------------------
@@ -454,13 +440,12 @@ module.exports = {
   */
 
   borderRadius: {
-    'none': '0',
-    'sm': '.125rem',
-    default: '.25rem',
-    'lg': '.5rem',
-    'full': '9999px',
+    none: "0",
+    sm: ".125rem",
+    default: ".25rem",
+    lg: ".5rem",
+    full: "9999px"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -483,36 +468,35 @@ module.exports = {
   */
 
   width: {
-    'auto': 'auto',
-    'px': '1px',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '10': '2.5rem',
-    '12': '3rem',
-    '16': '4rem',
-    '24': '6rem',
-    '32': '8rem',
-    '48': '12rem',
-    '64': '16rem',
-    '1/2': '50%',
-    '1/3': '33.33333%',
-    '2/3': '66.66667%',
-    '1/4': '25%',
-    '3/4': '75%',
-    '1/5': '20%',
-    '2/5': '40%',
-    '3/5': '60%',
-    '4/5': '80%',
-    '1/6': '16.66667%',
-    '5/6': '83.33333%',
-    'full': '100%',
-    'screen': '100vw'
+    auto: "auto",
+    px: "1px",
+    "1": "0.25rem",
+    "2": "0.5rem",
+    "3": "0.75rem",
+    "4": "1rem",
+    "6": "1.5rem",
+    "8": "2rem",
+    "10": "2.5rem",
+    "12": "3rem",
+    "16": "4rem",
+    "24": "6rem",
+    "32": "8rem",
+    "48": "12rem",
+    "64": "16rem",
+    "1/2": "50%",
+    "1/3": "33.33333%",
+    "2/3": "66.66667%",
+    "1/4": "25%",
+    "3/4": "75%",
+    "1/5": "20%",
+    "2/5": "40%",
+    "3/5": "60%",
+    "4/5": "80%",
+    "1/6": "16.66667%",
+    "5/6": "83.33333%",
+    full: "100%",
+    screen: "100vw"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -530,25 +514,24 @@ module.exports = {
   */
 
   height: {
-    'auto': 'auto',
-    'px': '1px',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
-    '10': '2.5rem',
-    '12': '3rem',
-    '16': '4rem',
-    '24': '6rem',
-    '32': '8rem',
-    '48': '12rem',
-    '64': '16rem',
-    'full': '100%',
-    'screen': '100vh'
+    auto: "auto",
+    px: "1px",
+    "1": "0.25rem",
+    "2": "0.5rem",
+    "3": "0.75rem",
+    "4": "1rem",
+    "6": "1.5rem",
+    "8": "2rem",
+    "10": "2.5rem",
+    "12": "3rem",
+    "16": "4rem",
+    "24": "6rem",
+    "32": "8rem",
+    "48": "12rem",
+    "64": "16rem",
+    full: "100%",
+    screen: "100vh"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -565,10 +548,9 @@ module.exports = {
   */
 
   minWidth: {
-    '0': '0',
-    'full': '100%',
+    "0": "0",
+    full: "100%"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -585,11 +567,10 @@ module.exports = {
   */
 
   minHeight: {
-    '0': '0',
-    'full': '100%',
-    'screen': '100vh'
+    "0": "0",
+    full: "100%",
+    screen: "100vh"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -607,18 +588,17 @@ module.exports = {
   */
 
   maxWidth: {
-    'xs': '20rem',
-    'sm': '30rem',
-    'md': '40rem',
-    'lg': '50rem',
-    'xl': '60rem',
-    '2xl': '70rem',
-    '3xl': '80rem',
-    '4xl': '90rem',
-    '5xl': '100rem',
-    'full': '100%',
+    xs: "20rem",
+    sm: "30rem",
+    md: "40rem",
+    lg: "50rem",
+    xl: "60rem",
+    "2xl": "70rem",
+    "3xl": "80rem",
+    "4xl": "90rem",
+    "5xl": "100rem",
+    full: "100%"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -635,10 +615,9 @@ module.exports = {
   */
 
   maxHeight: {
-    'full': '100%',
-    'screen': '100vh',
+    full: "100%",
+    screen: "100vh"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -656,16 +635,15 @@ module.exports = {
   */
 
   padding: {
-    'px': '1px',
-    '0': '0',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
+    px: "1px",
+    "0": "0",
+    "1": "0.25rem",
+    "2": "0.5rem",
+    "3": "0.75rem",
+    "4": "1rem",
+    "6": "1.5rem",
+    "8": "2rem"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -683,17 +661,16 @@ module.exports = {
   */
 
   margin: {
-    'auto': 'auto',
-    'px': '1px',
-    '0': '0',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
+    auto: "auto",
+    px: "1px",
+    "0": "0",
+    "1": "0.25rem",
+    "2": "0.5rem",
+    "3": "0.75rem",
+    "4": "1rem",
+    "6": "1.5rem",
+    "8": "2rem"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -711,16 +688,15 @@ module.exports = {
   */
 
   negativeMargin: {
-    'px': '1px',
-    '0': '0',
-    '1': '0.25rem',
-    '2': '0.5rem',
-    '3': '0.75rem',
-    '4': '1rem',
-    '6': '1.5rem',
-    '8': '2rem',
+    px: "1px",
+    "0": "0",
+    "1": "0.25rem",
+    "2": "0.5rem",
+    "3": "0.75rem",
+    "4": "1rem",
+    "6": "1.5rem",
+    "8": "2rem"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -739,13 +715,12 @@ module.exports = {
   */
 
   shadows: {
-    default: '0 2px 4px 0 rgba(0,0,0,0.10)',
-    'md': '0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)',
-    'lg': '0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)',
-    'inner': 'inset 0 2px 4px 0 rgba(0,0,0,0.06)',
-    'none': 'none',
+    default: "0 2px 4px 0 rgba(0,0,0,0.10)",
+    md: "0 4px 8px 0 rgba(0,0,0,0.12), 0 2px 4px 0 rgba(0,0,0,0.08)",
+    lg: "0 15px 30px 0 rgba(0,0,0,0.11), 0 5px 15px 0 rgba(0,0,0,0.08)",
+    inner: "inset 0 2px 4px 0 rgba(0,0,0,0.06)",
+    none: "none"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -761,15 +736,14 @@ module.exports = {
   */
 
   zIndex: {
-    'auto': 'auto',
-    '0': 0,
-    '10': 10,
-    '20': 20,
-    '30': 30,
-    '40': 40,
-    '50': 50,
+    auto: "auto",
+    "0": 0,
+    "10": 10,
+    "20": 20,
+    "30": 30,
+    "40": 40,
+    "50": 50
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -785,13 +759,12 @@ module.exports = {
   */
 
   opacity: {
-    '0': '0',
-    '25': '.25',
-    '50': '.5',
-    '75': '.75',
-    '100': '1',
+    "0": "0",
+    "25": ".25",
+    "50": ".5",
+    "75": ".75",
+    "100": "1"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -808,9 +781,8 @@ module.exports = {
   */
 
   svgFill: {
-    'current': 'currentColor',
+    current: "currentColor"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -827,9 +799,8 @@ module.exports = {
   */
 
   svgStroke: {
-    'current': 'currentColor',
+    current: "currentColor"
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -846,53 +817,52 @@ module.exports = {
   */
 
   modules: {
-    appearance: ['responsive'],
-    backgroundAttachment: ['responsive'],
-    backgroundColors: ['responsive', 'hover'],
-    backgroundPosition: ['responsive'],
-    backgroundRepeat: ['responsive'],
-    backgroundSize: ['responsive'],
-    borderColors: ['responsive', 'hover'],
-    borderRadius: ['responsive'],
-    borderStyle: ['responsive'],
-    borderWidths: ['responsive'],
-    cursor: ['responsive'],
-    display: ['responsive'],
-    flexbox: ['responsive'],
-    float: ['responsive'],
-    fonts: ['responsive'],
-    fontWeights: ['responsive', 'hover'],
-    height: ['responsive'],
-    leading: ['responsive'],
-    lists: ['responsive'],
-    margin: ['responsive'],
-    maxHeight: ['responsive'],
-    maxWidth: ['responsive'],
-    minHeight: ['responsive'],
-    minWidth: ['responsive'],
-    negativeMargin: ['responsive'],
-    opacity: ['responsive', 'hover'],
-    overflow: ['responsive'],
-    padding: ['responsive'],
-    pointerEvents: ['responsive'],
-    position: ['responsive'],
-    resize: ['responsive'],
-    shadows: ['responsive'],
+    appearance: ["responsive"],
+    backgroundAttachment: ["responsive"],
+    backgroundColors: ["responsive", "hover"],
+    backgroundPosition: ["responsive"],
+    backgroundRepeat: ["responsive"],
+    backgroundSize: ["responsive"],
+    borderColors: ["responsive", "hover"],
+    borderRadius: ["responsive"],
+    borderStyle: ["responsive"],
+    borderWidths: ["responsive"],
+    cursor: ["responsive"],
+    display: ["responsive"],
+    flexbox: ["responsive"],
+    float: ["responsive"],
+    fonts: ["responsive"],
+    fontWeights: ["responsive", "hover"],
+    height: ["responsive"],
+    leading: ["responsive"],
+    lists: ["responsive"],
+    margin: ["responsive"],
+    maxHeight: ["responsive"],
+    maxWidth: ["responsive"],
+    minHeight: ["responsive"],
+    minWidth: ["responsive"],
+    negativeMargin: ["responsive"],
+    opacity: ["responsive", "hover"],
+    overflow: ["responsive"],
+    padding: ["responsive"],
+    pointerEvents: ["responsive"],
+    position: ["responsive"],
+    resize: ["responsive"],
+    shadows: ["responsive"],
     svgFill: [],
     svgStroke: [],
-    textAlign: ['responsive'],
-    textColors: ['responsive', 'hover'],
-    textSizes: ['responsive'],
-    textStyle: ['responsive', 'hover'],
-    tracking: ['responsive'],
-    userSelect: ['responsive'],
-    verticalAlign: ['responsive'],
-    visibility: ['responsive'],
-    whitespace: ['responsive'],
-    width: ['responsive'],
-    zIndex: ['responsive'],
+    textAlign: ["responsive"],
+    textColors: ["responsive", "hover"],
+    textSizes: ["responsive"],
+    textStyle: ["responsive", "hover"],
+    tracking: ["responsive"],
+    userSelect: ["responsive"],
+    verticalAlign: ["responsive"],
+    visibility: ["responsive"],
+    whitespace: ["responsive"],
+    width: ["responsive"],
+    zIndex: ["responsive"]
   },
-
 
   /*
   |-----------------------------------------------------------------------------
@@ -905,9 +875,8 @@ module.exports = {
   */
 
   options: {
-    prefix: '',
+    prefix: "",
     important: false,
-    separator: ':',
-  },
-
-}
+    separator: ":"
+  }
+};
