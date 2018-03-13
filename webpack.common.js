dce65ea8cce3d33b766c5cdfc359cf65f65b2695
budparr <budparr@gmail.com>
2018-03-13 10:18:23 -0400

    Refactor Webpack config to better handle CSS

--- webpack.common.js
+++ webpack.common.js
@@ -1,23 +1,60 @@
-const path = require("path");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require("path")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = {
   entry: {
-    app: "./src/index.js"
+    app: "./src/index.js",
     // algolia: "./src/algolia.js"
   },
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+                minimize: true || {
+                  discardComments: {
+                    removeAll: true,
+                  },
+                  minifyFontValues: false,
+                  autoprefixer: false,
+                },
+              },
+            },
+            {
+              loader: "postcss-loader",
+              options: {
+                ident: "postcss",
+                plugins: () => [
+                  // require('postcss-flexbugs-fixes'),
+                  // autoprefixer({
+                  //   flexbox: 'no-2009',
+                  // }),
+                  require("postcss-import"),
+                  require("tailwindcss")("./src/css/tailwind-config.js"),
+                  require("autoprefixer"),
+                ],
+              },
+            },
+          ],
+        }),
+      },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["env"]
+            presets: ["env"],
             // plugins: [require('babel-plugin-transform-object-rest-spread')]
-          }
-        }
+          },
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
@@ -28,38 +65,38 @@ module.exports = {
               // “publicPath” is used by several Webpack’s plugins to update the URLs inside CSS, HTML files when generating production builds.
               publicPath: "/dist/Fonts",
               // “path” tells  Webpack where it should store the result
-              outputPath: "/Fonts/"
-            }
-          }
-        ]
+              outputPath: "/Fonts/",
+            },
+          },
+        ],
       },
       {
         //Mustache loader for Algolia templates
         test: /\.html$/,
-        loader: "mustache-loader"
+        loader: "mustache-loader",
         // loader: 'mustache-loader?minify'
         // loader: 'mustache-loader?{ minify: { removeComments: false } }'
         // loader: 'mustache-loader?noShortcut'
-      }
-    ]
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(
       [
         "./static/dist/js/*.js",
         "./static/dist/css/*.css",
-        "./static/dist/Fonts/*.*"
+        "./static/dist/Fonts/*.*",
       ],
 
       {
         root: __dirname,
         verbose: true,
         dry: false,
-        allowExternal: true
+        allowExternal: true,
       }
-    )
+    ),
   ],
   output: {
-    path: path.join(__dirname, "./static/dist/")
-  }
-};
+    path: path.join(__dirname, "./static/dist/"),
+  },
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,77 +1,25 @@
 const webpack = require("webpack")
 const merge = require("webpack-merge")
 const common = require("./webpack.common.js")
-
-const postCSSLoaderOptions = {
-  ident: "postcss",
-  plugins: () => [
-    // require('postcss-flexbugs-fixes'),
-    // autoprefixer({
-    //   flexbox: 'no-2009',
-    // }),
-    require("postcss-import"),
-    require("tailwindcss")("./src/css/tailwind-config.js"),
-    require("autoprefixer"),
-  ],
-}
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
 
 module.exports = merge(common, {
   devtool: "inline-source-map",
-  module: {
-    rules: [
-      // "postcss" loader applies autoprefixer to our CSS.
-      // "css" loader resolves paths in CSS and adds assets as dependencies.
-      // "style" loader turns CSS into JS modules that inject <style> tags.
-      // In production, we use a plugin to extract that CSS to a file, but
-      // in development "style" loader enables hot editing of CSS.
-      // By default we support CSS Modules with the extension .module.css
-      {
-        test: /\.css$/,
-        exclude: /\.module\.css$/,
-        use: [
-          "style-loader",
 
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-              sourceMap: true,
-              minimize: true || {
-                discardComments: {
-                  removeAll: false,
-                },
-                minifyFontValues: false,
-                autoprefixer: false,
-              },
-            },
-          },
-          {
-            loader: "postcss-loader",
-            options: postCSSLoaderOptions,
-          },
-
-          // require.resolve("style-loader"),
-          // {
-          //   loader: require.resolve("css-loader"),
-          //   options: {
-          //     importLoaders: 1,
-          //   },
-          // },
-          // {
-          //   loader: require.resolve("postcss-loader"),
-          //   options: postCSSLoaderOptions,
-          // },
-        ],
-      },
-    ],
-  },
   plugins: [
     // define production === true to selectivily call scripts
     new webpack.DefinePlugin({
       PRODUCTION: JSON.stringify(false),
     }),
+    // In production, hash our CSS, here we do not
+    new ExtractTextPlugin({
+      filename: getPath => {
+        return getPath("css/[name].dev.css")
+      },
+      allChunks: true,
+    }),
   ],
   output: {
-    filename: "js/[name].js",
+    filename: "js/[name].dev.js",
   },
 })
