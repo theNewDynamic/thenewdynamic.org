7f930115e79288f5432bacff9fd39bda7ac14989
budparr <budparr@gmail.com>
2018-04-07 11:34:52 -0400

    Add quick test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // require("typeface-lato")
-import styles from "./css/styles.css";
-import fonts from "./fonts.css";
-import "./js/main.js";
+import styles from "./css/styles.css"
+import fonts from "./fonts.css"
+import "./js/main.js"
+
+console.log(PRODUCTION)
