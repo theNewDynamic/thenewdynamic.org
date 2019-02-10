eb5a40174136ec636513452be885e53bab39ed63
budparr <budparr@gmail.com>
2019-02-10 17:32:18 -0500

    Update scripts and styles

---
+++ assets/css/tailwind/plugins/css-grid.js
@@ -0,0 +1,53 @@
+const _ = require('lodash')
+
+module.exports = function ({ grids = _.range(1, 12), gaps = {}, variants = ['responsive']}) {
+  return function ({ e, addUtilities }) {
+    addUtilities([
+      { '.grid': { display: 'grid' } },
+      { '.grid-dense': { gridAutoFlow: 'dense' } },
+      ..._.map(gaps, (size, name) => ({
+        [`.${e(`grid-gap-${name}`)}`]: { gridGap: size },
+      })),
+      ...grids.map(columns => ({
+        [`.grid-columns-${columns}`]: {
+          gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        }
+      })),
+      ..._.range(1, _.max(grids) + 1).map(span => ({
+        [`.col-span-${span}`]: {
+          gridColumnStart: `span ${span}`,
+        }
+      })),
+      ..._.range(1, _.max(grids) + 2).map(line => ({
+        [`.col-start-${line}`]: {
+          gridColumnStart: `${line}`,
+        },
+        [`.col-end-${line}`]: {
+          gridColumnEnd: `${line}`,
+        },
+      })),
+      ...grids.map(columns => ({
+        [`.grid-rows-${columns}`]: {
+          gridTemplateRows: `repeat(${columns}, 1fr)`,
+        }
+      })),
+      ..._.map(gaps, (size, name) => ({
+        [`.${e(`grid-row-gap-${name}`)}`]: { gridRowGap: size },
+      })),
+      ..._.range(1, _.max(grids) + 1).map(span => ({
+        [`.row-span-${span}`]: {
+          gridRow: `span ${span}`,
+        }
+      }
+      )),
+      ..._.range(1, _.max(grids) + 2).map(line => ({
+        [`.row-start-${line}`]: {
+          gridRowStart: `${line}`,
+        },
+        [`.row-end-${line}`]: {
+          gridRowEnd: `${line}`,
+        },
+      })),
+    ], variants)
+  }
+}
--- assets/index.js
+++ assets/index.js
@@ -1,8 +1,11 @@
 // To use https://github.com/KyleAMathews/typefaces
-//require("typeface-vesper-libre")
-import styles from "./css/styles.css"
+//require("typeface-roboto")
+import "typeface-roboto"
+
+//import styles from "./css/index.css"
 import "./js/lazysizes.js"
 import "./js/quicklink.js"
 import "./js/nojs.js"
 
 
+console.log('env', process.env.NODE_ENV)
