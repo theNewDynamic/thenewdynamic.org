b9c507511afd45cf7a63338281a9d2efee0057c0
budparr <budparr@gmail.com>
2020-04-05 12:22:13 -0400

    Organize Asset files

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 // To use https://github.com/KyleAMathews/typefaces
-import "typeface-nunito";
-import "./css/styles.css";
+// import "typeface-nunito";
+
 
 import "./js/polyfills.js"; //MUST GO AT TOP
 import "./js/lazysizes.js";
