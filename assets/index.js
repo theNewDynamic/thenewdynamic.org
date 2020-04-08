00a3cfa32ebd0e7c2a2940fa7b881c7c30b03990
budparr <budparr@gmail.com>
2020-04-08 13:05:55 -0400

    Hide db scripts for now

--- assets/index.js
+++ assets/index.js
@@ -7,17 +7,17 @@ import "./js/lazysizes.js";
 import "./js/quicklink.js";
 import "./js/nojs.js";
 
-import Vue from "vue";
-import App from "./js/app/filters.vue";
+// import Vue from "vue";
+// import App from "./js/app/filters.vue";
 
-// FILTERS APP
-if (document.getElementById("app")) { 
+// // FILTERS APP
+// if (document.getElementById("app")) { 
 
-	new Vue({
-		el: "#app",
-		components: { App }
-	});
-} 
-console.log("NODE_ENV=", process.env.NODE_ENV);
+// 	new Vue({
+// 		el: "#app",
+// 		components: { App }
+// 	});
+// } 
+console.log("NODE_ENVg=", process.env.NODE_ENV);
 
 // TODO: Add back --experimental-scope-hoisting to parcel command after upgrade to 1.13 (if path issue is fixed)
