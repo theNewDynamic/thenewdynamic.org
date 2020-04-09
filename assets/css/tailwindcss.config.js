31d91f8e237734fd60626a8cd1ae47cdba9d7d66
budparr <budparr@gmail.com>
2020-04-08 22:25:25 -0400

    [wip] Update Tailwind classes

--- assets/css/tailwindcss.config.js
+++ assets/css/tailwindcss.config.js
@@ -1,5 +1,61 @@
 module.exports = {
 	theme: {
+		fontFamily: {
+			brand: [
+				"URW Franklin Gothic W01",
+				"URW Franklin Gothic",
+				"system-ui",
+				"BlinkMacSystemFont",
+				"-apple-system",
+				"Segoe UI",
+				"Roboto",
+				"Oxygen",
+				"Ubuntu",
+				"Cantarell",
+				"Fira Sans",
+				"Droid Sans",
+				"Helvetica Neue",
+				"sans-serif"
+			],
+			sans: [
+				"Nunito",
+				"Avenir Next W00",
+				"Avenir Next",
+				"Avenir",
+				"system-ui",
+				"BlinkMacSystemFont",
+				"-apple-system",
+				"Segoe UI",
+				"Roboto",
+				"Oxygen",
+				"Ubuntu",
+				"Cantarell",
+				"Fira Sans",
+				"Droid Sans",
+				"Helvetica Neue",
+				"sans-serif"
+			],
+			serif: [
+				"Constantia",
+				"Lucida Bright",
+				"Lucidabright",
+				"Lucida Serif",
+				"Lucida",
+				"DejaVu Serif",
+				"Bitstream Vera Serif",
+				"Liberation Serif",
+				"Georgia",
+				"serif"
+			],
+			mono: [
+				"Menlo",
+				"Monaco",
+				"Consolas",
+				"Liberation Mono",
+				"Courier New",
+				"monospace"
+			]
+		},
 		extend: {
 			colors: {
 				"primary-color": {
@@ -11,11 +67,11 @@ module.exports = {
 					600: "#A30832",
 					700: "#6D0522",
 					800: "#510419",
-					900: "#360311",
-				},
-			},
-		},
+					900: "#360311"
+				}
+			}
+		}
 	},
 	variants: {},
-	plugins: [],
+	plugins: []
 };
