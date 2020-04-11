2a8c1edd51ba0c36d7507ac7c7b82754d3c8f411
budparr <budparr@gmail.com>
2020-04-11 12:26:38 -0400

    [wip] Add Algolia

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,36 @@
 // To use https://github.com/KyleAMathews/typefaces
 // import "typeface-nunito";
 
-
 import "./js/polyfills.js"; //MUST GO AT TOP
-import 'alpinejs'
+import "alpinejs";
 import "./js/search/index.js";
 import "./js/lazysizes.js";
 import "./js/quicklink.js";
 import "./js/nojs.js";
 
+// window.onload {
+// 	var getParams = function (url) {
+// 		var params = {};
+// 		var parser = document.createElement("a");
+// 		parser.href = url;
+// 		var query = parser.search.substring(1);
+// 		var vars = query.split("&");
+// 		for (var i = 0; i < vars.length; i++) {
+// 			var pair = vars[i].split("=");
+// 			params[pair[0]] = decodeURIComponent(pair[1]);
+// 		}
+// 		return params;
+// 	};
 
-console.log("NODE_ENV=", process.env.NODE_ENV);
+// 	getParams(window.location.href);
+
+// 	// Get parameters from a URL string
+// 	var url = window.location.href;
+// 	const result = url + getParams(url);
+
+// 	fb_share.href =
+// 		"https://twitter.com/intent/tweet?text=Jamstack%20Tools%20at%20The%20New%20Dynamic&url=" +
+// 		result;
+// };
+
+// console.log("url", result);
--- assets/js/search/index.js
+++ assets/js/search/index.js
@@ -1,7 +1,12 @@
 import algoliasearch from "algoliasearch/lite";
 import instantsearch from "instantsearch.js";
-import { searchBox, hits, clearRefinements, refinementList } from "instantsearch.js/es/widgets";
-// import item from "item.html";
+import {
+	searchBox,
+	hits,
+	clearRefinements,
+	refinementList,
+	poweredBy,
+} from "instantsearch.js/es/widgets";
 
 const searchClient = algoliasearch(
 	"EGKL8BPJHJ",
@@ -14,6 +19,10 @@ const search = instantsearch({
 	// TODO routing: searchRouting,
 	searchClient,
 });
+
+const refinementTemplate = `<a href="{{url}}" style="{{#isRefined}}font-weight: bold{{/isRefined}}">
+{{label}} <span class="text-xs text-gray-600">({{count}})</span>
+</a>`;
 
 search.addWidgets([
 	searchBox({
@@ -31,59 +40,86 @@ search.addWidgets([
 
 	hits({
 		container: "#hits",
-		// template: item
+		cssClasses: {
+			item: "tool_type",
+		},
 		templates: {
-			empty: '<h3 class="text-2xl">No results for <q>{{ query }}</q></h3>',
-			item: `
-			  <div class="">      
-			  <div>{{tool_type}}</div>
-                <div class="">
-                  {{title}}
-                </div>
-                <div class="hit-description">
-                  {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
-                </div>                
-              </div>
-            `,
+			empty(results) {
+				return `<h3 class="text-2xl">No results for <q>${results.query}</q></h3>`;
+			},
+			item(hit) {
+				return `
+        <a href="${hit.uri}" class="block w-full h-full no-underline shadow-md hover:shadow-xl">
+          <div class="${hit.primary_section}">
+            <div class="px-2 py-1 text-xs">${hit.tool_type}</div>
+          </div>
+            <div class="p-3">
+            <h2 class="font-bold text-xl text-gray-800 leading-tight mb-2">          
+              ${hit.title}
+            </h2>
+            <p class="text-sm text-gray-700">${hit.description}</p>      
+            </div>    
+          </a>
+        `;
+			},
 		},
 	}),
 	clearRefinements({
-		container: '#clear-refinements',
-	  }),
+		container: "#clear-refinements",
+		templates: {
+			resetLabel: "Clear Filters",
+		},
+	}),
 	refinementList({
 		container: "#refinement-tool_type",
 		attribute: "tool_type",
-		sortBy: ['name:asc','count:desc'],
+		sortBy: ["name:asc", "count:desc"],
 		limit: 25,
+		// templates: {
+		// 	item: refinementTemplate,
+		// },
 	}),
 	refinementList({
 		container: "#refinement-interaction_type",
 		attribute: "interaction_type",
-		sortBy: ['name:asc','count:desc'],
+		sortBy: ["name:asc", "count:desc"],
 		limit: 25,
+		// templates: {
+		// 	item: refinementTemplate,
+		// },
 	}),
 	refinementList({
 		container: "#refinement-data_model",
 		attribute: "data_model",
-		sortBy: ['name:asc','count:desc'],
+		sortBy: ["name:asc", "count:desc"],
 		limit: 25,
+		// templates: {
+		// 	item: refinementTemplate,
+		// },
 	}),
 	refinementList({
 		container: "#refinement-license",
 		attribute: "license",
-		sortBy: ['name:asc','count:desc'],
+		sortBy: ["name:asc", "count:desc"],
 		limit: 25,
+		// templates: {
+		// 	item: refinementTemplate,
+		// },
 	}),
 	refinementList({
 		container: "#refinement-tags",
 		attribute: "tags",
-		sortBy: ['name:asc','count:desc'],
+		sortBy: ["name:asc", "count:desc"],
 		showMore: true,
 		showMoreLimit: 50,
 		limit: 10,
+		// templates: {
+		// 	item: refinementTemplate,
+		// },
 	}),
-
-	
+	poweredBy({
+		container: "#powered-by",
+	}),
 ]);
 
 search.start();
