1bb5e977f8a288b2652a547dbc9f60d87c926d19
budparr <budparr@gmail.com>
2020-05-15 08:56:05 -0400

    Fix undefined value in search script

--- assets/js/search/index.js
+++ assets/js/search/index.js
@@ -21,6 +21,8 @@ const search = instantsearch({
 const refinementTemplate = `<a href="{{url}}" class="block w-full hover:bg-gray-400 px-3 -ml-3 py-1 text-gray-700{{#isRefined}} font-bold{{/isRefined}}">{{label}} <span class="text-xs text-gray-600 font-normal">({{count}})</span>
 </a>`;
 
+let timerId;
+
 search.addWidgets([
   searchBox({
     container: "#searchbox",
