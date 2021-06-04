5d640926b721a0a7ac203ac90d761078370a3a9d
Regis Philibert <login@regisphilibert.com>
2021-06-04 11:53:23 -0400

    Set operator to 'and' for refinementLists

--- assets/tnd-search/instantsearch/widgets.js
+++ assets/tnd-search/instantsearch/widgets.js
@@ -19,6 +19,7 @@ export let tndWidgets = {
       item: `<a href="{{url}}" class="block w-full hover:bg-gray-400 px-3 -ml-3 py-1 text-gray-700{{#isRefined}} font-bold{{/isRefined}}">{{label}} <span class="text-xs text-gray-600 font-normal">({{count}})</span>
       </a>`,
       sortBy: ["name:asc", "count:desc"],
+      operator: 'and'
     },
   }
 }
